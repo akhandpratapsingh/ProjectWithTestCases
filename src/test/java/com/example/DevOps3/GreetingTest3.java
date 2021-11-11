d5fa97e54f52d62651e7fd1b8def0e9403fb31ec
Akhand Pratap Singh <akhandpratap.singh@servicenow.com>
2021-11-11 19:11:44 +0530

    adding more packagesof test

---
+++ src/test/java/com/example/DevOps3/GreetingTest3.java
@@ -0,0 +1,42 @@
+package com.example.EnterpriseDevops;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class GreetingTest extends EnterpriseDevopsApplicationTests {
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@BeforeAll
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	public void testAnushGreet() throws Exception {
+		mockMvc.perform(get("/greeting?name=Anush")).andExpect(status().isOk())
+				.andExpect(content().contentType("application/json"))
+				.andExpect(jsonPath("$.name").value("Anush")).andExpect(jsonPath("$.day").value("Monday"));
+	}
+
+	@Test
+	public void testKumarGreet() throws Exception {
+		mockMvc.perform(get("/greeting?name=Kumar")).andExpect(status().isOk())
+				.andExpect(content().contentType("application/json"))
+				.andExpect(jsonPath("$.name").value("Kumar")).andExpect(jsonPath("$.day").value("Monday"));
+	}
+}
