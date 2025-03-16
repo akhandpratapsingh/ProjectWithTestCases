3f54de255acf4fc81f3bc0225578c9cce1f80482
akhand pratap singh <akhand123singh@gmail.com>
2025-03-16 15:09:00 +0530

    Update GreetingTest2.java

--- src/test/java/com/example/Devops2/GreetingTest2.java
+++ src/test/java/com/example/Devops2/GreetingTest2.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
