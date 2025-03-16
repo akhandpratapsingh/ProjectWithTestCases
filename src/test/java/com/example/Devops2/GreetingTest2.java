e70c3e13dc8069eca88cbfdd9b71da3be6036cb8
akhand pratap singh <akhand123singh@gmail.com>
2025-03-16 15:21:50 +0530

    Update GreetingTest2.java

--- src/test/java/com/example/Devops2/GreetingTest2.java
+++ src/test/java/com/example/Devops2/GreetingTest2.java
@@ -13,8 +13,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import com.example.EnterpriseDevops.EnterpriseDevopsApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = EnterpriseDevopsApplication.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class GreetingTest2 extends EnterpriseDevopsApplicationTests2 {
 
