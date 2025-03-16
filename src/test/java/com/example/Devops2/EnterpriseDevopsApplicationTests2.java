e0fc5c6b1220c0c7b6d458db47e19b7b27ab0f40
akhand pratap singh <akhand123singh@gmail.com>
2025-03-16 15:14:16 +0530

    Update EnterpriseDevopsApplicationTests2.java

--- src/test/java/com/example/Devops2/EnterpriseDevopsApplicationTests2.java
+++ src/test/java/com/example/Devops2/EnterpriseDevopsApplicationTests2.java
@@ -1,7 +1,18 @@
 package com.example.Devops2;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest(classes = EnterpriseDevopsApplication.class)
 class EnterpriseDevopsApplicationTests2 {
