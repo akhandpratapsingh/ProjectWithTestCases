7ad442b88a0002ae3121d3baae89580f52067571
akhand pratap singh <akhand123singh@gmail.com>
2025-03-16 15:19:00 +0530

    Update GreetingTest3.java

--- src/test/java/com/example/DevOps3/GreetingTest3.java
+++ src/test/java/com/example/DevOps3/GreetingTest3.java
@@ -12,7 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class GreetingTest3 extends EnterpriseDevopsApplicationTests3 {
 
