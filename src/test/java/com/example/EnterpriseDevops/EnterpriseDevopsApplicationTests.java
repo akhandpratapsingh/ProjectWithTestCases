d0eb2756d08fd4da2ad45b5a124e245664c807a3
akhand pratap singh <akhand123singh@gmail.com>
2025-03-16 15:19:50 +0530

    Update EnterpriseDevopsApplicationTests.java

--- src/test/java/com/example/EnterpriseDevops/EnterpriseDevopsApplicationTests.java
+++ src/test/java/com/example/EnterpriseDevops/EnterpriseDevopsApplicationTests.java
@@ -1,9 +1,21 @@
 package com.example.EnterpriseDevops;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import com.example.EnterpriseDevops.EnterpriseDevopsApplication;
+
+@SpringBootTest(classes = EnterpriseDevopsApplication.class)
 class EnterpriseDevopsApplicationTests {
 
 	@Test
