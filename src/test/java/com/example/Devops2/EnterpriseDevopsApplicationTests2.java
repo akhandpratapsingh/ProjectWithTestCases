ec679a8a358937b69b91cd70f00277550175d7d4
akhand pratap singh <akhand123singh@gmail.com>
2025-03-16 15:13:32 +0530

    Update EnterpriseDevopsApplicationTests2.java

--- src/test/java/com/example/Devops2/EnterpriseDevopsApplicationTests2.java
+++ src/test/java/com/example/Devops2/EnterpriseDevopsApplicationTests2.java
@@ -3,7 +3,7 @@ package com.example.Devops2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = EnterpriseDevopsApplication.class)
 class EnterpriseDevopsApplicationTests2 {
 
 	@Test
